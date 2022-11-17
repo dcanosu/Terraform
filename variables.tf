c41bfaafac54448dcceb08adbe81467b171b18d1
Daniel C <107519984+dcanosu@users.noreply.github.com>
2022-11-17 16:08:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ variable "region" {
   default ="us-east-1"
 }
 
-variable "my_access_key" {
-    description = "Access-key-for-AWS"
-    default = ""
-  }
+#variable "my_access_key" {
+#    description = "Access-key-for-AWS"
+#    default = ""
+#  }
   
-  variable "my_secret_key" {
-    description = "Secret-key-for-AWS"
-    default = ""
-  }
+#  variable "my_secret_key" {
+ #   description = "Secret-key-for-AWS"
+#    default = ""
+#  }
